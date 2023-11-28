11b2ac4f463b48d6a51b360cd8ee2f01e04497de
czavidowski <127987006+czavidowski@users.noreply.github.com>
2023-11-27 21:07:02 -0300

    Update main.cpp
    ahora si lo lee bien

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -5,6 +5,47 @@
 
 int main() {
 
+    ifstream archi2;
+    archi2.open("iriClientesGYM.csv", ios::in);
+    int n = 0;
+    sCliente* clientes = nullptr;
+    if (!archi2.is_open()) {
+        return -1;
+    }
+
+    string header;
+    getline(archi2, header);  // Leer la línea de encabezado y descartarla
+
+    char coma = ',';  // Separador
+    string line;
+    while (getline(archi2, line)) {
+        resizeClientes(clientes, n);
+        sCliente nuevoCliente;
+        std::stringstream iss(line);
+        //sCliente* nuevoCliente = &clientes[n];  // Corregir el acceso al nuevo cliente
+        string auxn,auxa,auxe,auxtel;
+        // Lee los campos de la línea
+        iss >> nuevoCliente.idCliente >> coma;
+        getline(iss, auxn, coma);  //ACA SALTA EL ERROR
+        getline(iss, auxa, coma);
+        getline(iss, auxe, coma);
+        getline(iss, auxtel, coma);
+        string fechaNacStr;
+        iss >> fechaNacStr >> coma;
+        nuevoCliente.fechaNac = ConvertirFechaATime_t(fechaNacStr);
+        iss >>nuevoCliente.estado >> coma;
+        nuevoCliente.nombre=auxn;
+        nuevoCliente.apellido=auxa;
+        nuevoCliente.email=auxe;
+        nuevoCliente.telefono=auxtel;
+        clientes[n-1]=nuevoCliente;
+
+        // Incrementar el contador
+
+    }
+
+    archi2.close();
+
     ifstream archi;
     archi.open("iriClasesGYM.csv", ios::in);
     int N = 0;
@@ -19,7 +60,6 @@ int main() {
     while (getline(archi, linea)) {
         // Redimensionar el arreglo dinámico
         resizeTipos(tipos, N);
-
         stringstream ss(linea);  // Asignar la línea al stringstream
         char coma;
 
@@ -27,46 +67,9 @@ int main() {
         ss >> tipos[N].idClase >> coma >> tipos[N].nombreClase >> coma >> tipos[N].horario;
 
         // Incrementar el contador
-        N++;
+
     }
     archi.close();
-
-    ifstream archi2;
-    archi2.open("iriClientesGYM.csv", ios::in);
-    int n = 0;
-    sCliente* clientes = new sCliente[n];
-    if (!archi2.is_open()) {
-        return -1;
-    }
-
-    string header;
-    getline(archi2, header);  // Leer la línea de encabezado y descartarla
-
-    char coma = ',';  // Separador
-    string line;
-    while (getline(archi2, line)) {
-        resizeClientes(clientes, n);
-
-        std::stringstream iss(line);
-        sCliente& nuevoCliente = clientes[n];  // Corregir el acceso al nuevo cliente
-
-        // Lee los campos de la línea
-        iss >> nuevoCliente.idCliente >> coma;
-        getline(iss, nuevoCliente.nombre, coma);  //ACA SALTA EL ERROR
-        getline(iss, nuevoCliente.apellido, coma);
-        getline(iss, nuevoCliente.email, coma);
-        getline(iss, nuevoCliente.telefono, coma);
-        string fechaNacStr;
-        iss >> fechaNacStr >> coma;
-        nuevoCliente.fechaNac = ConvertirFechaATime_t(fechaNacStr);
-        iss >> nuevoCliente.estado >> coma;
-
-        // Incrementar el contador
-        n++;
-    }
-
-    archi2.close();
-
 
     sTipo* tiposNuevo=new sTipo[N];
     Actualizar_estructura(tipos,tiposNuevo,N);
